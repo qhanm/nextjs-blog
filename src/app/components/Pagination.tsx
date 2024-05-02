616895d3e8b492c4c4d73c640e8123896a1ae2d6
nam@mtms.live <nam@mtms.live>
2024-05-02 09:58:58 +0700

    pagination

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import styles from "../page.module.css";
+import { useRouter } from "next/navigation";
 
 type TPaginationProps = {
   totalRecords: number;
@@ -10,19 +13,40 @@ type TPaginationProps = {
 function Pagination(props: TPaginationProps) {
   const { totalRecords, page, limit } = props;
   const totalPages = Math.ceil(+totalRecords / +limit);
+  const arrPages = new Array(totalPages).fill(undefined);
 
+  const router = useRouter();
+  console.log(router);
   return (
     <div className={styles.paginationContainer}>
       <div className={styles.pagination}>
-        <Link href="/">&laquo;</Link>
+        <Link
+          href={`/post?page=${+page - 1}&limit=${limit}`}
+          style={{
+            pointerEvents: +page <= 1 ? "none" : "auto",
+          }}
+        >
+          &laquo;
+        </Link>
 
-        {new Array(totalPages).map((_, index: number) => (
-          <Link href="/" key={`${String(index)}`}>
+        {arrPages.map((_, index: number) => (
+          <Link
+            href={`/post?page=${index + 1}&limit=${limit}`}
+            key={`${String(index)}`}
+            className={+page === index + 1 ? styles.active : ""}
+          >
             {index + 1}
           </Link>
         ))}
 
-        <Link href="/">&raquo;</Link>
+        <Link
+          href={`/post?page=${+page + 1}&limit=${limit}`}
+          style={{
+            pointerEvents: +page >= totalPages ? "none" : "auto",
+          }}
+        >
+          &raquo;
+        </Link>
       </div>
     </div>
   );
